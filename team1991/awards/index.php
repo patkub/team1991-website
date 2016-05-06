dae87214c46a285e8695f9f16ff36535971ebd63
Patka <epicpatka@gmail.com>
2016-05-05 21:46:44 -0400

    expandable awards
    expandable awards with descriptions

--- team1991/awards/index.php
+++ team1991/awards/index.php
@@ -84,60 +84,132 @@
 					</div>
 				</div>
 				
-				<div class="card">
+				<!-- 2015 -->
+				<div class="card teal lighten-4">
 					<div class="card-content">
-						<table class="responsive-table striped">
-							<thead>
-								<tr>
-									<th>Season</th>
-									<th>Event</th>
-									<th>Awards</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>2015</td>
-									<td>NE District - Hartford Event</td>
-									<td>Gracious Professionalism Award sponsored by Johnson & Johnson</td>
-								</tr>
-								<tr>
-									<td>2014</td>
-									<td>Groton District Event</td>
-									<td>District Event Winner</td>
-								</tr>
-								<tr>
-									<td>2013</td>
-									<td>BAE Systems Granite State Regional</td>
-									<td>Industrial Safety Award sponsored by Underwriters Laboratories</td>
-								</tr>
-								
-								<tr>
-									<td>2012</td>
-									<td>WPI Regional</td>
-									<td>Innovation in Control Award sponsored by Rockwell Automation<br>Coopertition Award</td>
-								</tr>
-								<tr>
-									<td>2012</td>
-									<td>Northeast Utilities FIRST Connecticut Regional</td>
-									<td>Innovation in Control Award sponsored by Rockwell Automation</td>
-								</tr>
-								<tr>
-									<td>2008</td>
-									<td>Connecticut Regional</td>
-									<td>Regional Finalists<br>Delphi "Driving Tomorrow's Technology" Award</td>
-								</tr>
-								<tr>
-									<td>2007</td>
-									<td>UTC Connecticut Regional</td>
-									<td>Website Award</td>
-								</tr>
-								<tr>
-									<td>2007</td>
-									<td>BAE SYSTEMS Granite State Regional</td>
-									<td>Rookie All Star Award<br>Highest Rookie Seed</td>
-								</tr>
-							</tbody>
-						</table>
+						<div class="header-content">
+							<span class="card-title"><h4>2015</h4></span>
+							<h5>NE District - Hartford Event</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Gracious Professionalism Award sponsored by Johnson & Johnson</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates outstanding demonstration of <i>FIRST</i> Core Values such as continuous Gracious Professionalism® working together both on and off the playing field.</p></div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				<!-- 2014 -->
+				<div class="card teal lighten-4">
+					<div class="card-content">
+						<div class="header-content">
+							<span class="card-title"><h4>2014</h4></span>
+							<h5>NE District - Hartford Event</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">District Event Winner</div>
+									<div class="collapsible-body grey lighten-3"><p>Winner of the Hartford competition together with teams 230 and 1699.</p></div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				<!-- 2013 -->
+				<div class="card teal lighten-4">
+					<div class="card-content">
+						<div class="header-content">
+							<span class="card-title"><h4>2013</h4></span>
+							<h5>BAE Systems Granite State Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Industrial Safety Award sponsored by Underwriters Laboratories</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates the team that progresses beyond safety fundamentals by using innovative ways to eliminate or protect against hazards.</p></div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				<!-- 2012 -->
+				<div class="card teal lighten-4">
+					<div class="card-content">
+						<div class="header-content">
+							<span class="card-title"><h4>2012</h4></span>
+							<h5>WPI Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Innovation in Control Award sponsored by Rockwell Automation</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates an innovative control system or application of control components – electrical, mechanical, or software – to provide unique machine functions.</p></div>
+								</li>
+								<li>
+									<div class="collapsible-header">Coopertition Award</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates outstanding demonstration of <i>FIRST</i> Core Values such as continuous Gracious Professionalism® working together both on and off the playing field.</p></div>
+								</li>
+							</ul>
+							
+							<h5>Northeast Utilities <i>FIRST</i> Connecticut Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Innovation in Control Award sponsored by Rockwell Automation</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates an innovative control system or application of control components – electrical, mechanical, or software – to provide unique machine functions.</p></div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				<!-- 2008 -->
+				<div class="card teal lighten-4">
+					<div class="card-content">
+						<div class="header-content">
+							<span class="card-title"><h4>2008</h4></span>
+							<h5>Connecticut Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Regional Finalists</div>
+									<div class="collapsible-body grey lighten-3"><p>
+									Finalists of the Connecticut Regional.
+									</p></div>
+								</li>
+								<li>
+									<div class="collapsible-header" style="cursor: default;">Delphi "Driving Tomorrow's Technology" Award</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				<!-- 2007 -->
+				<div class="card teal lighten-4">
+					<div class="card-content">
+						<div class="header-content">
+							<span class="card-title"><h4>2007</h4></span>
+							<h5>UTC Connecticut Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Website Award</div>
+									<div class="collapsible-body grey lighten-3"><p>The Website Award recognizes excellence in student-designed, built, and managed <i>FIRST</i> team websites.</p></div>
+								</li>
+								<li>
+									<div class="collapsible-header">Coopertition Award</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates outstanding demonstration of <i>FIRST</i> Core Values such as continuous Gracious Professionalism® working together both on and off the playing field.</p></div>
+								</li>
+							</ul>
+							
+							<h5>BAE SYSTEMS Granite State Regional</h5>
+							<ul class="collapsible" data-collapsible="expandable">
+								<li>
+									<div class="collapsible-header">Rookie All Star Award</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates the rookie team exemplifying a young but strong partnership effort, as well as implementing the mission of <i>FIRST</i> to inspire students to learn more about science and technology.</p></div>
+								</li>
+								<li>
+									<div class="collapsible-header">Highest Rookie Seed</div>
+									<div class="collapsible-body grey lighten-3"><p>Celebrates the highest-seeded rookie team at the conclusion of the qualifying rounds.</p></div>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
